b1bfd4a1d1c7c76e06b2c8cf0da8dab6824ee653
Paulo Henrique Araujo de Andrade <122685733+PauloAraujo84@users.noreply.github.com>
2025-07-10 23:40:10 -0300

    Add files via upload

---
+++ Smartphone.cs
@@ -0,0 +1,30 @@
+namespace DesafioPOO.Models
+{
+    public abstract class Smartphone
+    {
+        public string Numero { get; set; }
+        public string Modelo { get; protected set; }
+        public string IMEI { get; protected set; }
+        public int Memoria { get; protected set; }
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+        }
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+
+        public abstract void InstalarAplicativo(string nomeApp);
+    }
+}
